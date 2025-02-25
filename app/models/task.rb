0d7e785e452941dc79d061f36b969a44cd7346c9
pratiksau <pratik.sau@bigbinary.com>
2025-02-25 15:34:12 +0530

    Added git hooks, eslint, prettier and rubocop

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Task < ApplicationRecord
   MAX_TITLE_LENGTH = 125
   VALID_TITLE_REGEX = /\A.*[a-zA-Z0-9\s].*\z/i
   validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH },
-                    format: { with: VALID_TITLE_REGEX}
+    format: { with: VALID_TITLE_REGEX }
 end
