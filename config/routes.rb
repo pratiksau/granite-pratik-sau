0d10afdad12fa3d9a4aad8dfb01e1b9de6e40a02
pratiksau <pratik.sau@bigbinary.com>
2025-02-26 10:03:52 +0530

    Added API based architecture

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   resources :tasks, only: :index
-
+  root "home#index"
+  get "*path", to: "home#index", via: :all
   # Defines the root path route ("/")
   # root "posts#index"
 end
