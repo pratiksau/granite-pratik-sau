72ef94bb8e0268b6c7805480820c2aea3be1ecc0
pratiksau <pratik.sau@bigbinary.com>
2025-03-24 10:14:31 +0530

    Solved neeto-deploy related granite issues

--- config/initializers/sqlite_custom_regexp_function.rb
+++ config/initializers/sqlite_custom_regexp_function.rb
@@ -8,9 +8,9 @@ if defined?(ActiveRecord::ConnectionAdapters::SQLite3Adapter)
       original_initialize(*args)
 
       raw_connection.create_function("regexp", 2) do |function, pattern, expression|
-          regex_matcher = Regexp.new(pattern.to_s, Regexp::IGNORECASE)
-          function.result = expression.to_s.match(regex_matcher) ? 1 : 0
-        end
+        regex_matcher = Regexp.new(pattern.to_s, Regexp::IGNORECASE)
+        function.result = expression.to_s.match(regex_matcher) ? 1 : 0
       end
+    end
   end
 end
